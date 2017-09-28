c0202dd449ba2e07c9fa356fa047488fed16878b
zub <crazysal.14@gmail.com>
2017-09-27 22:28:11 -0400

    test photos

--- release/code/driver.m
+++ release/code/driver.m
@@ -1,20 +1,20 @@
-% % %Driver file to Run All other files
-% % % 
-% 
-% % % %Initial var for running files 
-% img = imread('../data/testy/3.jpg');
-% filterBank = createFilterBank(); 
-% %% Run Q 1.1
-% computeDictionary();
-% load('dictionary.mat');
-% %% Run Q1.3
-% [wordMap] = getVisualWords(img, filterBank, dictionary);
-% imagesc(wordMap)
-%  
-% %% Run Q2.1  
-% [dictionarySize,~] = size(dictionary);
-% [histo1] = getImageFeatures(wordMap, dictionarySize);
-% histogram(histo1);
+% %Driver file to Run All other files
+% % 
+
+% % %Initial var for running files 
+img = imread('../data/testy/3.jpg');
+filterBank = createFilterBank(); 
+%% Run Q 1.1
+computeDictionary();
+load('dictionary.mat');
+%% Run Q1.3
+[wordMap] = getVisualWords(img, filterBank, dictionary);
+imagesc(wordMap)
+ 
+%% Run Q2.1  
+[dictionarySize,~] = size(dictionary);
+[histo1] = getImageFeatures(wordMap, dictionarySize);
+histogram(histo1);
         
 %% Run Q2.2  
 L = 3; 
